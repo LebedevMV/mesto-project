c6b8b30c63cb3e8fbb30f24f02b49b75e6db700b
Maxim Lebedev <lebedev.maximv@yandex.ru>
2021-11-17 21:53:39 +0300

    First review corrections

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const content = document.querySelector(".page");
-
 // Выбираем элементы для попапа редактирования профиля
 const popUpProfileEdit = content.querySelector("#edit-profile");
 const profileEditButton = content.querySelector(".profile__edit-button");
@@ -9,29 +8,6 @@ const profileBioEdit = popUpProfileEdit.querySelector(".pop-up__item_el_bio");
 const profileSubmitButton = popUpProfileEdit.querySelector(".pop-up__submit");
 const profileName = content.querySelector(".profile__name");
 const profileBio = content.querySelector(".profile__bio");
-
-// Функции для попапа редактирования профиля
-function profileOpen() {
-  popUpProfileEdit.classList.toggle("pop-up_is-closed");
-  profileNameEdit.value = profileName.textContent;
-  profileBioEdit.value = profileBio.textContent;
-}
-function profileClose() {
-  popUpProfileEdit.classList.toggle("pop-up_is-closed");
-}
-
-function profileSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = profileNameEdit.value;
-  profileBio.textContent = profileBioEdit.value;
-  profileClose();
-}
-
-// Слушатели событий для попапа редактирования профиля
-profileEditButton.addEventListener("click", profileOpen);
-profileEditCloseButton.addEventListener("click", profileClose);
-profileSubmitButton.addEventListener("click", profileSubmit);
-
 // Выбираем элементы для попапа добавления изображения
 const popUpAddImage = content.querySelector("#add-image");
 const addImageButton = content.querySelector(".profile__add-button");
@@ -41,42 +17,92 @@ const addImageImageEdit = popUpAddImage.querySelector(
   ".pop-up__item_el_image-link"
 );
 const addImageSubmitButton = popUpAddImage.querySelector(".pop-up__submit");
-const addImageName = content.querySelector(".profile__name");
-const addImageBio = content.querySelector(".profile__bio");
+//Выбираем элементы для попапа откпыимя изображения
+const popUpviewImage = content.querySelector("#view-image");
+const viewCloseButton = popUpviewImage.querySelector(".pop-up__close");
 
-// Функции для попапа добавления изображения
-function addImageToggle() {
-  popUpAddImage.classList.toggle("pop-up_is-closed");
+// Функции для попапов
+function popUpOpen(popUp) {
+  popUp.classList.remove("pop-up_is-closed");
+}
+function popUpClose(popUp) {
+  popUp.classList.add("pop-up_is-closed");
 }
 
-function addImageSubmit() {
+// Функции попапа редактирования профиля
+function profileEdit() {
+  profileNameEdit.value = profileName.textContent;
+  profileBioEdit.value = profileBio.textContent;
+  popUpOpen(popUpProfileEdit);
+}
+
+function profileSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileNameEdit.value;
+  profileBio.textContent = profileBioEdit.value;
+  popUpClose(popUpProfileEdit);
+}
+
+function profileClose(evt) {
+  popUpClose(popUpProfileEdit);
+}
+
+// Функции попапа добавления изображения
+function addImageOpen() {
+  popUpOpen(popUpAddImage);
+}
+
+function addImageClose() {
+  popUpClose(popUpAddImage);
+}
+
+function addImageSubmit(evt) {
+  evt.preventDefault();
+  const postLink = addImageImageEdit.value;
+  const postName = addImageTitleEdit.value;
+  createCard(postName, postLink);
+  popUpClose(popUpAddImage);
+  return images.prepend(newPost);
+}
+
+// Слушатели попапа редактирования профиля
+profileEditButton.addEventListener("click", profileEdit);
+profileEditCloseButton.addEventListener("click", profileClose);
+popUpProfileEdit.addEventListener("submit", profileSubmit);
+// Слушатели попапа добавления изображения
+addImageButton.addEventListener("click", addImageOpen);
+addImageCloseButton.addEventListener("click", addImageClose);
+popUpAddImage.addEventListener("submit", addImageSubmit);
+
+// Подгрузка карточек
+
+function createCard(name, link) {
   const images = content.querySelector(".elements");
   const post = content.querySelector("#card").content;
   const newPost = post.querySelector(".card").cloneNode(true);
-  newPost.querySelector(".card__image").src = addImageImageEdit.value;
-  newPost.querySelector(".card__image").alt = addImageTitleEdit.value;
-  newPost.querySelector(".card__text").textContent = addImageTitleEdit.value;
-  images.prepend(newPost);
-  addImageToggle();
-
+  newPost.querySelector(".card__image").src = link;
+  newPost.querySelector(".card__image").alt = name;
+  newPost.querySelector(".card__text").textContent = name;
+  // Лайк карточек
   const postLikeButton = newPost.querySelector(".card__like");
-  console.log(postLikeButton);
   postLikeButton.addEventListener("click", function (evt) {
     const eventTarget = evt.target;
     eventTarget.classList.toggle("card__like_active");
   });
-
+  // Удаление карточек
   const postDeleteButton = newPost.querySelector(".card__delete-button");
   postDeleteButton.addEventListener("click", function (evt) {
     const eventTarget = evt.target;
-    const postToDelete = eventTarget.parentElement;
-    postToDelete.remove();
+
+    const postDelete = eventTarget.parentElement;
+    postDelete.remove();
   });
+
+  // Открытие изображения
 
   const popUpviewImage = content.querySelector("#view-image");
   const imageClick = newPost.querySelector(".card__image");
-  imageClick.addEventListener("click", function (evt) {
-    const eventTarget = evt.target;
+  imageClick.addEventListener("click", function () {
     const imageContent = content.querySelector(".pop-up__view-image");
     imageContent.src = newPost.querySelector(".card__image").src;
     imageContent.alt = newPost.querySelector(".card__text").textContent;
@@ -87,12 +113,9 @@ function addImageSubmit() {
   return images.prepend(newPost);
 }
 
-// Слушатели событий для попапа редактирования профиля
-addImageButton.addEventListener("click", addImageToggle);
-addImageCloseButton.addEventListener("click", addImageToggle);
-addImageSubmitButton.addEventListener("click", addImageSubmit);
-
-// Подгрузка карточек
+viewCloseButton.addEventListener("click", function () {
+  popUpviewImage.classList.toggle("pop-up_is-closed");
+});
 
 const initialCards = [
   {
@@ -121,48 +144,14 @@ const initialCards = [
   },
 ];
 
+// Подгрузка карточек
+
 function getImages(cards) {
   cards.forEach(function (item) {
-    const images = content.querySelector(".elements");
-    const post = content.querySelector("#card").content;
-    const newPost = post.querySelector(".card").cloneNode(true);
-    newPost.querySelector(".card__image").src = item.link;
-    newPost.querySelector(".card__image").alt = item.name;
-    newPost.querySelector(".card__text").textContent = item.name;
-    // Лайк карточек
-    const postLikeButton = newPost.querySelector(".card__like");
-    postLikeButton.addEventListener("click", function (evt) {
-      const eventTarget = evt.target;
-      eventTarget.classList.toggle("card__like_active");
-    });
-    // Удаление карточек
-    const postDeleteButton = newPost.querySelector(".card__delete-button");
-    postDeleteButton.addEventListener("click", function (evt) {
-      const eventTarget = evt.target;
-
-      const postDelete = eventTarget.parentElement;
-      postDelete.remove();
-    });
-
-    // Открытие изображения
-    const popUpviewImage = content.querySelector("#view-image");
-    const imageClick = newPost.querySelector(".card__image");
-    imageClick.addEventListener("click", function (evt) {
-      const imageContent = content.querySelector(".pop-up__view-image");
-      imageContent.src = newPost.querySelector(".card__image").src;
-      imageContent.alt = newPost.querySelector(".card__text").textContent;
-      const imageTitle = content.querySelector(".pop-up__image-title");
-      imageTitle.textContent = newPost.querySelector(".card__text").textContent;
-      popUpviewImage.classList.toggle("pop-up_is-closed");
-    });
-    return images.prepend(newPost);
+    const postLink = item.link;
+    const postName = item.name;
+    createCard(postName, postLink);
   });
 }
 
 getImages(initialCards);
-
-const popUpviewImage = content.querySelector("#view-image");
-const viewCloseButton = popUpviewImage.querySelector(".pop-up__close");
-viewCloseButton.addEventListener("click", function () {
-  popUpviewImage.classList.toggle("pop-up_is-closed");
-});
